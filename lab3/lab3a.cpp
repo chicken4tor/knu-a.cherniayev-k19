b48971ff777c3a05bf3ade2d99fea66271294322
chicken4tor <andreych237@gmail.com>
2021-04-05 22:16:07 +0300

    Lab 3a
    * combined sort
    * benchmark mode

--- lab3/lab3a.cpp
+++ lab3/lab3a.cpp
@@ -32,14 +32,23 @@
 #include <algorithm>
 #include <chrono>
 #include <random>
+#include <iomanip>
 
 #include "labdemo.h"
 
 
 using namespace std;
 
+
+// Генеруємо випадкові данні
+random_device r;
+default_random_engine reng(r());
+
 // Будемо показувати проміжні кроки?
 static bool trace_on = true;
+
+// Benchmark
+typedef chrono::time_point<chrono::steady_clock> bench_time;
 
 // Порівняння рядків
 bool compare_greater(const string &s1, const string &s2)
@@ -125,6 +134,11 @@ int hoare_partition(string *A, int lo, int hi)
         }
 
         swap(A[i], A[j]);
+
+        if (trace_on)
+        {
+            print_array(A + lo, A + hi);
+        }
     }
 
     return -1;
@@ -142,7 +156,17 @@ void quicksort(string *A, int lo, int hi)
 
 void quick_sort(string *first, string *last)
 {
+    if (trace_on)
+    {
+        print_array(first, last);
+    }
+
     quicksort(first, 0, last - first - 1);
+
+    if (trace_on)
+    {
+        print_array(first, last);
+    }
 }
 
 // Merge sort. Top-down, small lists
@@ -190,6 +214,12 @@ mrg_list mergesort(mrg_list &m)
     left = mergesort(left);
     right = mergesort(right);
 
+    if (trace_on)
+    {
+        print_array(&left[0], &left[0] + left.size());
+        print_array(&right[0], &right[0] + right.size());
+    }
+
     return merge(left, right);
 }
 
@@ -204,10 +234,79 @@ void merge_sort(string *first, string *last)
     m = mergesort(m);
 
     copy(begin(m), end(m), first);
+
+    if (trace_on)
+    {
+        print_array(first, last);
+    }
 }
 
-void combi_sort(string *first, string *last)
-{}
+// Combined sort
+
+const int THRESHOLD = 10;
+
+int partition(string *A, int lo, int hi)
+{
+    const string &pivot = A[hi];
+    int i, j;
+    i = lo;
+    j = lo;
+
+    for (int i = lo; i < hi; ++i)
+    {
+        if (compare_lesser(A[i], pivot))
+        {
+            swap(A[i], A[j]);
+            j += 1;
+        }
+    }
+
+    swap(A[j], A[hi]);
+
+    return j;
+}
+
+void insertion_quick_sort(string *A, int lo, int hi)
+{
+    while (lo < hi)
+    {
+        if (hi - lo + 1 < THRESHOLD)
+        {
+            insertion_sort(A + lo, A + hi + 1);
+            break;
+        }
+        else
+        {
+            int p = partition(A, lo, hi);
+
+            if (p - lo < hi - p)
+            {
+                insertion_quick_sort(A, lo, p - 1);
+                lo = p + 1;
+            }
+            else
+            {
+                insertion_quick_sort(A, p + 1, hi);
+                hi = p - 1;
+            }
+        }
+    }
+}
+
+void combined_sort(string *first, string *last)
+{
+    insertion_quick_sort(first, 0, last - first - 1);
+
+    if (trace_on)
+    {
+        print_array(first, last);
+    }
+}
+
+void system_sort(string *first, string *last)
+{
+    sort(first, last, compare_lesser);
+}
 
 class Lab3Demo : public LabDemo
 {
@@ -234,14 +333,50 @@ protected:
             }
         }
         else if (verb == "trace")
-        {}
+        {
+            if (args.size())
+            {
+                if (args[0] == "on")
+                {
+                    trace_on = true;
+                }
+                else if (args[0] == "off")
+                {
+                    trace_on = false;
+                }
+                else
+                {
+                    cout << "Незрозумілий аргумент - " << args[0] << endl;
+                }
+            }
+            else
+            {
+                cout << boolalpha << trace_on << endl;
+            }
+        }
         else if (verb == "benchmark")
-        {}
+        {
+            long n = 10000;
+
+            if (args.size() == 1)
+            {
+                n = strtol(args[0].c_str(), nullptr, 10);
+            }
+
+            if (n != 0)
+            {
+                benchmark(n);
+            }
+            else
+            {
+                cout << "Потрібно вказати кількість елементів" << endl;
+            }
+        }
         else if (verb == "help")
         {
             cout << "Демонстрація реалізованніх алгоритмів - sort N\n"
                     "Показувати проміжні кроки - trace on або off\n"
-                    "Режим вимірювання ефективності - benchmark <кількість елементів>\n"
+                    "Режим вимірювання ефективності - benchmark <кількість елементів, 10 000 за замовчанням>\n"
                     "Вихід - quit\n"
                     "Ця інформація - help\n";
         }
@@ -253,19 +388,15 @@ protected:
         return true;
     }
 
-    void sort_alg(long n)
+    void data_gen(vector<string> &data, long n)
     {
-        vector<string> data;
-
+        data.clear();
         data.reserve(n);
-
-        // Генеруємо випадкові данні
-        default_random_engine reng;
 
         const char alphabet[] = "0123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm!+;:)(_";
 
         // Довжина слів
-        uniform_int_distribution<string::size_type> word_len(1, 18);
+        uniform_int_distribution<string::size_type> word_len(1, 9);
         // Випадкові літери із абетки
         uniform_int_distribution<size_t> litera(0, end(alphabet) - begin(alphabet) - 1);
 
@@ -284,11 +415,17 @@ protected:
 
             data.push_back(word);
         }
+    }
+
+    void sort_alg(long n)
+    {
+        vector<string> data;
+
+        data_gen(data, n);
 
         vector<string> dup;
 
         dup = data;
-
         cout << "Insertion sort\n";
         insertion_sort(&dup[0], &dup[0] + dup.size());
 
@@ -301,8 +438,84 @@ protected:
         merge_sort(&dup[0], &dup[0] + dup.size());
 
         dup = data;
-        cout << "Combi sort\n";
-        combi_sort(&dup[0], &dup[0] + dup.size());
+        cout << "Combined sort\n";
+        combined_sort(&dup[0], &dup[0] + dup.size());
+    }
+
+    void benchmark(long n)
+    {
+        vector<string> data;
+
+        data_gen(data, n);
+
+        bool trace_state = trace_on;
+
+        // Не виводимо проміжні кроки
+        trace_on = false;
+
+        bench_time start;
+        bench_time end;
+        chrono::duration<double, milli> bench_result;
+
+        vector<string> dup;
+
+        struct BenchmarkInfo
+        {
+            void (*sort_fun)(string *, string *);
+            string alg_desc;
+        };
+
+        BenchmarkInfo bi[] = {
+            {insertion_sort, "Insertion"},
+            {quick_sort, "Quick"},
+            {merge_sort, "Merge"},
+            {combined_sort, "Combined"},
+            {system_sort, "STL"},
+        };
+
+        cout << "Random dataset\n";
+        for (auto &b : bi)
+        {
+            dup = data;
+
+            start = chrono::steady_clock::now();
+
+            b.sort_fun(&dup[0], &dup[0] + dup.size());
+
+            end = chrono::steady_clock::now();
+
+            bench_result = end - start;
+
+            cout  << left << setw(16) << b.alg_desc + " sort: " << n << ", " << fixed << setw(10) << right << setprecision(4) << bench_result.count() << " ms\n";
+        }
+
+        // Зробимо трішкі не відсортований масив
+        data = dup;
+
+        if (data.size() > 5)
+        {
+            swap(data[1], data[data.size() - 2]);
+            swap(data[3], data[data.size() - 5]);
+        }
+
+        cout << "Almost sorted dataset\n";
+        for (auto &b : bi)
+        {
+            dup = data;
+
+            start = chrono::steady_clock::now();
+
+            b.sort_fun(&dup[0], &dup[0] + dup.size());
+
+            end = chrono::steady_clock::now();
+
+            bench_result = end - start;
+
+            cout  << left << setw(16) << b.alg_desc + " sort: " << n << ", " << fixed << setw(10) << right << setprecision(4) << bench_result.count() << " ms\n";
+        }
+
+        // Повертаємо старе значення
+        trace_on = trace_state;
     }
 
 private:
@@ -310,11 +523,12 @@ private:
 
 int main()
 {
-    // string a1[] = { "AA", "B", "A", "AB", "X", "SSS", "Nnn", "a", "AAAA", "NNN"};
+    // string a1[] = { "AA", "B", "A", "AB", "X", "SSS", "Nnn", "a", "AAAA", "NNN", "zs", "oo", "d", "c", "ddd", "mmm", "lk", "ZS", "dh", "hg", "5t"};
 
     // insertion_sort(begin(a1), end(a1));
     // quick_sort(begin(a1), end(a1));
     // merge_sort(begin(a1), end(a1));
+    // combined_sort(begin(a1), end(a1));
 
     // bool sep = false;
 
